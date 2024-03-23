c088d14a05b4ec0805419345fcd5c60d01a71e3e
Felipe Silva <felipe.allware@gmail.com>
2024-03-23 12:42:56 -0300

    feat: Criando rotas para CRUD de Users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function __construct(User $model, Request $request) {
+        $this->model = $model;
+        $this->request = $request;
+    }
+
+    public function store(Request $request) {
+        $request->validate(User::rules());
+
+        $data = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $request->validate(User::rulesUpdate());
+
+        $user = Auth::user();
+
+        $data = $request->all();
+        $data['password'] = Hash::make($data['password']);
+
+        $user->update($data);
+
+        return response()->json($data, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Auth
 Route::post('/login', [AuthController::class, 'login']);
 
+// User
+Route::post('/user', [UserController::class, 'store']);
+
 Route::group(['middleware' => 'auth:api'], function () {
+    // User
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
     // Auth
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh-token', [AuthController::class, 'refresh']);
