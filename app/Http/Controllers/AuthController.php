d868dfd428a4bef8064fb5627978ee3ee804d24e
Felipe Silva <felipe.allware@gmail.com>
2024-03-23 12:21:51 -0300

    feat: Criando recursos para autenticação com token JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login() {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(["message" => "Credenciais inválidas."], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me() {
+        return response()->json(auth()->user());
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        return response()->json(["message" => "Logout efetuado com sucesso."]);
+    }
+
+    public function refresh() {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token) {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Auth
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh-token', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
