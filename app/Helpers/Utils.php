a55ab82c08d3f80b318bb082d049bce272f6faa5
felipe.silva <felipe.silva@pwi.com.br>
2024-04-18 13:46:34 -0300

    feat: Criando método para transformar um array de Model para um array de uma DTO

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -2,11 +2,30 @@
 
 namespace App\Helpers;
 
+use App\Contracts\DtoInterface;
+use Exception;
+
 class Utils
 {
     public static function floatToStringBankFormat(float $value): string {
         $formattedValue = (string) ($value * 100);
         
         return $formattedValue;
+    }
+
+    public static function modelArrayToDtoArray(array $modelArray, string $dtoClass): array {
+        if (!$dtoClass instanceof DtoInterface) {
+            throw new Exception("A classe {$dtoClass} não implementa a interface DtoInterface. Não é possível utilizar o método {DtoInterface::class}");
+        }
+
+        $dtoArray = [];
+
+        foreach ($modelArray as $model) {
+            $dto = $dtoClass::fromModel($model);
+
+            array_push($dtoArray, $dto);
+        }
+        
+        return $dtoArray; 
     }
 }
